c783d670f7ec16c90b3d5b2951360a7f67790716
GeorgeBotos03 <gbotos80@yahoo.ro>
2024-12-04 18:22:50 +0200

    added new files

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Heading } from "./common";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <>hello world</>
+    <Heading>Hello world</Heading>
   </StrictMode>
 );
