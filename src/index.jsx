1804f725e43b6e54e0cf5d21f3ba034e80067167
GeorgeBotos03 <gbotos80@yahoo.ro>
2024-12-09 17:28:01 +0200

    implemented text component

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,29 @@
-import PropTypes from "prop-types";
-import Container from "./index.styled";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Button, DropdownField, Heading, TextField } from "./common";
+import TwitterIcon from "./assets/twitter.svg?react";
 
-const TemplateComponent = (props) => {
-  return <Container>Our nice template component {props.children}</Container>;
-};
-
-TemplateComponent.propTypes = {
-  children: PropTypes.element,
-};
-
-export default TemplateComponent;
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Heading>Hello world</Heading>
+    <Button variant="fill">Click me</Button>
+    <TextField
+      iconChildren={<TwitterIcon />}
+      isIconVisible={true}
+      errorChildren={"Link invalid"}
+      isErrorVisible={true}
+      onChange={(e) => console.log(e.target.value)}
+    />
+    <DropdownField
+      iconChildren={<TwitterIcon />}
+      isIconVisible={true}
+      options={[
+        { id: 1, value: 10, name: "Option 1" },
+        { id: 2, value: 11, name: "Option 2" },
+        { id: 3, value: 12, name: "Option 3" },
+      ]}
+      onChange={(e) => console.log(e.target.value)}
+    />
+  </StrictMode>
+);
